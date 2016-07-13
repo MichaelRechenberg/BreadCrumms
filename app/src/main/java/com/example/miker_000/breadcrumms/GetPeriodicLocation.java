9680aaae02a0d7e33c0e7e87a2e72e8035ebe7cc
RechenBall <michaelrechenberg@oawl.net>
2016-07-13 16:06:10 -0500

    As of this commit you can log current location and start/stop service via binding/unbinding from service.  The location is logged even if the phone is locked or you are on another app.
    However, the service still runs if the activity is swiped off of the "Recent Apps" screen...I have yet to find official documentation regarding the behavior or what lifecycle method to implement to handle that.

--- app/src/main/java/com/example/miker_000/breadcrumms/GetPeriodicLocation.java
+++ app/src/main/java/com/example/miker_000/breadcrumms/GetPeriodicLocation.java
@@ -1,62 +1,151 @@
 package com.example.miker_000.breadcrumms;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
+import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.util.Log;
-import android.widget.Toast;
 
-public class GetPeriodicLocation extends Service {
-    private Messenger messenger = new Messenger(new LocationHandler());
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
+/**
+ * Service that creates Google API connection and sends location to LocationReceiver
+ *C
+ */
+public class GetPeriodicLocation extends Service implements
+        GoogleApiClient.ConnectionCallbacks{
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private PendingIntent locationIntent;
+    private LocationSettingsRequest.Builder locationSettingsRequest;
+
 
     public GetPeriodicLocation() {
 
     }
 
-    /**
-     * Defines Handler for Messenger
-     * TODO: Make this in a separate Java Class file
-     */
-    private class LocationHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            //Log.d("IT WORKED", "NANANANNANAN BATMAN");
-            Toast.makeText(getApplicationContext(), "From Service", Toast.LENGTH_SHORT).show();
-        }
-    };
-
+    //Connect to Google API and create location request
     @Override
     public IBinder onBind(Intent intent) {
         Log.d("Derp", "onBind() called");
-        Messenger temp = intent.getParcelableExtra("messenger");
-        messenger =  new Messenger (temp.getBinder());
-        //This is just here for testing
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Message msg = Message.obtain(null, 0, 0, 0);
-                try{
-                    messenger.send(msg);
-                }
-                catch (RemoteException e){
-                    e.printStackTrace();
-                    Log.e("Derp", "Fucked Up");
-                }
-            }
-        }, 2000);
-
-        return messenger.getBinder();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setFastestInterval(5000)
+                .setInterval(5000);
+        //have pendingIntent send broadcast that will be picked up
+        //  by LocationReceiver
+        Intent tempIntent = new Intent(this, LocationReceiver.class);
+        //TODO: Change Flag to Cancel Current?
+        locationIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                LocationReceiver.LOCATION_UPDATE_CODE, tempIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        locationSettingsRequest = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        googleApiClient.connect();
+        //Local Binder stuff to start/stop service via public methods???
+        return new Binder();
     }
 
+    //Disconnect from Google API and remove location updates
     @Override
     public boolean onUnbind(Intent intent) {
-        messenger = null;
-        return super.onUnbind(intent);
+        boolean superUnbind = super.onUnbind(intent);
+
+        removeMyLocationUpdates();
+        if(googleApiClient != null){
+            googleApiClient.disconnect();
+        }
+        return superUnbind;
+
     }
 
+
+    //Overriden Methods for Google API client
+
+    //add location update
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        addMyLocationUpdates();
+    }
+
+    //remove the location update
+    @Override
+    public void onConnectionSuspended(int i) {
+
+        removeMyLocationUpdates();
+    }
+
+
+    //Adds my location update request to LocationServices
+    private void addMyLocationUpdates(){
+        //Check if we have the settings for making request,
+        //TODO: Add dialog to request for settings later
+        PendingResult<LocationSettingsResult> result =
+            LocationServices.SettingsApi.checkLocationSettings(
+                googleApiClient,
+                locationSettingsRequest.build()
+            );
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch(status.getStatusCode()){
+                    //All location settings were satisfied
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        try{
+                            //Do security exception thing here
+                            LocationServices.FusedLocationApi.requestLocationUpdates(
+                                    googleApiClient,
+                                    locationRequest,
+                                    locationIntent
+                            );
+                            Log.d("Derp", "Location request added");
+                        }
+                        catch (SecurityException e){
+                            //pass silently
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.d("Derp", "Need to ask for permission");
+                        //TODO: Bring up dialog
+                        //status.startResolutionForResult(); ACTIVITY STUFF
+                        break;
+                }
+            }
+        });
+
+
+
+    }
+
+    //Removes the location updates requested by this service
+    private void removeMyLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                googleApiClient,
+                locationIntent
+        );
+        Log.d("Derp", "Location Request Removed");
+    }
 }
--- app/src/main/java/com/example/miker_000/breadcrumms/MainActivity.java
+++ app/src/main/java/com/example/miker_000/breadcrumms/MainActivity.java
@@ -1,59 +1,34 @@
 package com.example.miker_000.breadcrumms;
 
-import android.Manifest;
-import android.app.PendingIntent;
+
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
+
 import android.content.ServiceConnection;
-import android.content.pm.PackageManager;
+
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Handler;
+
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import android.widget.ToggleButton;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.common.internal.GetServiceRequest;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation = null;
     //indicates if we are currently making location updates
     private boolean active = false;
     private TextView latitudeData;
     private TextView longitudeData;
     private final int REQUEST_FOR_LOCATION = 1;
-    private Messenger messengerForService = null;
     //Inicates if this Activity is currently bound to GetPeriodicLocation Service
     private boolean isBound = false;
-
-    //Key used to index Messenger in GetPeriodicLocation service
-    private final String messengerKey = "com.mrechenberg.MainActivity";
 
 
     //Application Lifetime Methods
@@ -67,40 +42,22 @@ public class MainActivity extends AppCompatActivity {
         updateUI();
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
 
+    //Unbind from the service if the app is destroyed
     @Override
-    protected void onStop(){
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
         if(isBound){
             stopLocationMonitoring();
         }
+        Log.d("Derp", "onDestroy() called");
     }
 
-
-    /**
-     * Defines Handler for Messenger
-     * TODO: Make this in a separate Java Class file
-     */
-    private class LocationHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-            Log.d("IT WORKED", "NANANANNANAN BATMAN");
-            Toast.makeText(getApplicationContext(), "Activity received message", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-
-
-
+    //Used to connect to GetPeriodicLocation service
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder service) {
             Log.d("Derp", "Connected To Service");
-            //messengerForService = new Messenger(service);
             isBound = true;
 
         }
@@ -112,23 +69,20 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
     /*
-     * Retrieves the last location of the user and checks for permission
-     * TODO: Have some boolean value to check rather than check permission every time
-     *  Initalize the Service and Messenger for IPC
+     * Begins service GetPeriodicLocation to retrieve location updates
      */
     private void beginLocationMonitoring(){
-        messengerForService = new Messenger(new LocationHandler());
         Intent derp = new Intent()
-                .setClass(this, GetPeriodicLocation.class)
-                .putExtra("messenger", messengerForService);
-        Log.d("Derp", String.valueOf(bindService(derp, mConnection,
-                Context.BIND_AUTO_CREATE)));
+                .setClass(this, GetPeriodicLocation.class);
+        bindService(derp, mConnection, Context.BIND_AUTO_CREATE);
     }
 
     //Helper method that unbinds from service
     private void stopLocationMonitoring(){
         unbindService(mConnection);
+        Log.d("Derp", "unBound From service");
         isBound = false;
     }
 
